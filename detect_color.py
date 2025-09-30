e1134a9f8e770de26c6c3675e23480069c10fa34
El-Vo <vollrathe@t-online.de>
2025-09-30 11:38:59 +0200

    with example image

--- detect_color.py
+++ detect_color.py
@@ -20,8 +20,8 @@ boundaries = [
 # loop over the boundaries
 for (lower, upper) in boundaries:
 # create NumPy arrays from the boundaries
-lower = np.array(lower, dtype = "uint8")
-upper = np.array(upper, dtype = "uint8")
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
 # find the colors within the specified boundaries and apply
 # the mask
 mask = cv2.inRange(image, lower, upper)
@@ -29,3 +29,5 @@ output = cv2.bitwise_and(image, image, mask = mask)
 # show the images
 cv2.imshow("images", np.hstack([image, output]))
 cv2.waitKey(0) 
+
+# Tip: Try with rainbow image to find boundaries of color detection!
